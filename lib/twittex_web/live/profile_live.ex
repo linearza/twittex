0a169e642d5c50ee814a5c323d1603fccad4b75e
linearza <vanwijk.mc@gmail.com>
2025-03-18 07:29:26 +0200

    feat: install timex and render out tweeks for user

--- lib/twittex_web/live/profile_live.ex
+++ lib/twittex_web/live/profile_live.ex
@@ -2,9 +2,13 @@ defmodule TwittexWeb.ProfileLive do
   use TwittexWeb, :live_view
 
   alias Twittex.Accounts
+  alias Twittex.Feed
+
+  import TwittexWeb.FeedComponents
 
   def mount(%{"username" => username}, _session, socket) do
     user = Accounts.get_user_by_username!(username)
-    {:ok, assign(socket, :user, user)}
+    tweeks = Feed.list_tweeks_for_user(user)
+    {:ok, assign(socket, tweeks: tweeks, user: user)}
   end
 end
